94ceda90cbd31428fc090e91a5c448d9dd0ccf42
Chris Mann <chrispmann@gmail.com>
2022-09-07 09:30:22 -0700

    Change 2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const url = " http://localhost:3000/toys";
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  // makes sure the DOM is loaded
   const addBtn = document.querySelector("#new-toy-btn");
+  // adds the "add a new toy" button
   const toyFormContainer = document.querySelector(".container");
+  // creates an object for the toy container
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -19,11 +22,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Fetchers
 
-function getAllToys(url) {
-  return fetch(url)
-    .then((res) => res.json())
-    .then((toys) => {
+function getAllToys(url) { // loads all toys from index.json
+  return fetch(url) 
+    .then((res) => res.json()) 
+    .then((toys) => {  // the whole toy list is "toys" (only function scope)
       console.log(toys);
+      
     });
 }
 console.log(getAllToys(url))
+const toy = getAllToys(url)
+
+console.log(toy)
+
+
+function renderToyCard(cardData){
+  const card = document.createElement('li')
+
+})
